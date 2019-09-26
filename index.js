d579f99f7f0b32a208dc21fc1d98c41d114881a3
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-09-26 18:11:40 -0500

    se hace deploy con zeit now, se instalo libreria de now y express

--- index.js
+++ index.js
@@ -3,20 +3,26 @@
 
 //console.log(Mongo);
 
+/*
+
+
 const express = require('express');
 
-console.log(express);
+
 
  const app = express();
 
 app.listen(3001, function() {
-    console.log('listening on 3000')
+    console.log('listening on 3001')
   })
 
   app.get('/products', (req, res) => {
     res.send(gato)
   })
 
+  app.get('/', (req, res) => {
+    res.send("Utilice /products para checar existencias")
+  })
 
 
   var gato = {
@@ -47,4 +53,26 @@ app.listen(3001, function() {
 
   }
 
-  
+
+*/
+
+  const port = process.env.PORT || 3000
+
+var express = require("express");
+var app = express();
+app.use(express.json());
+
+app.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready On Server http://localhost:${port}`)
+});
+
+app.get("/get", (req, res, next) => {
+    res.json({
+        "version": process.env.VERSION
+    });
+});
+
+app.post('/post', function(request, response) {
+    response.send(request.body);
+});
