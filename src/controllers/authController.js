2e621d2b1ceef0094e3746248fa4eaa484797160
BrendaVielmas <breen.vielmas@gmail.com>
2019-09-26 18:14:26 -0500

    Prueba: Json para información de usuario, parte 2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 const router = Router();
 
-const User = require('../models/user');
+const User = require('../models/User');
 
-router.post('/signUp', (req, res, next) => {
+router.post('/signUp', async (req, res, next) => {
     const { username, email, password } = req.body;
    const user = new User ({
         username: username,
         email: email,
         password: password
     });
+    user.password = await user.encryptPassword(user.password);
     console.log(user)
     res.json({message: 'Received'})
 });
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,14 @@
 const {Schema, model } = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const userSchema = new Schema({
     username: String,
     email: String,
     password: String
 });
 
-model.exports = model('user', userSchema)
+userSchema.methods.encryptPassword =async (password) => {
+    const salt = await bcrypt.genSalt(10);
+     return bcrypt.hash(password, salt);
+};
+
+module.exports = model('user', userSchema)
