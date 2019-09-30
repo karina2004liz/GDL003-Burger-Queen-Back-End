de8052fa68e5b1981565c99bf0aba5b3e79edec9
Brenda Vielmas <breen.vielmas@gmail.com>
2019-09-29 23:54:44 -0500

    Se guardan datos en MongoDB.

--- src/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/prueba', {
-    userNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('Database is connected'))
--- src/index.js
+++ src/index.js
@@ -1,42 +1,100 @@
-const app = require('./app');
-require('./database');
+//const Mongo = require('mongodb');
+//console.log(Mongo);
+/*
+const express = require('express');
+ const app = express();
+app.listen(3001, function() {
+    console.log('listening on 3001')
+  })
+  app.get('/products', (req, res) => {
+    res.send(gato)
+  })
+  app.get('/', (req, res) => {
+    res.send("Utilice /products para checar existencias")
+  })
+*/
+'use strict'
 
-async function init() {
- await app.listen(3001);
- console.log('listening on 3001')
+const express = require ('express')
+const bodyParser = require ('body-parser')
+const mongoose = require ('mongoose')
 
-}
+const Product = require('./models/product')
 
-/*app.get('/products', (req, res) => {
-  res.send(gato)
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require("cors");
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+//app.use(express.json());
+
+app.listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready On Server http://localhost:${port}`)
+});
+
+app.get('/api/product', (req, res, next) => {
+    res.json({
+        "version": process.env.VERSION
+    });
+    res.send(200, {products: []})
+});
+
+app.get('api/product/:productId', (req, res) => {
+
 })
 
-var gato = {
+app.post('/api/product', (req, res) => {
+  console.log('POST /api/product')
+  console.log(req.body)
 
-  michi :{
-      name : "michi",
-      age : "5years"  
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
 
-  },
-  michi2 :{
-      name : "michi",
-      age : "5years"  
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({message: `Error while saving in database: ${err}`})
+    res.status(200).send({product: productStored})
+  })
+});
 
-  },
+app.put('/api/product/:productId', (req, res) => {
 
-  michi3 :{
-      name : "michi",
-      age : "5years"  
+})
 
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
+mongoose.connect('mongodb://localhost:27017/Nekoffee', (err, res) =>{
+  if (err) {
+    return console.log(`Error to connect with database: ${err}`)
   }
+})
 
-  ,    michi4 :{
-      name : "michi",
-      age : "5years"  
+let products = {
 
-  },
+ products: [
 
+    {name : "coffee",
+    price: 15,
+    id: 1,
+    },
+    {
+    name: "tea",
+    price: 10,
+    id: 2,
+    },
+    {name: "chocolate",
+    price: 20,
+    id: 3
 
-}*/
+    }
 
-init();
+  ]
+}
