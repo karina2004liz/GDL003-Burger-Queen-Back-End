4bcd9d9490d2f9bce0469d42c283a82f9e3c5613
Karina2004liz <eng.karina.cabrera@gmail.com>
2019-10-05 21:35:05 -0500

    se estiliza codigo y detalles de conexion

--- src/userServices/middleware.js
+++ src/userServices/middleware.js
@@ -1,23 +1,19 @@
-var jwt = require('jwt-simple'); 
-var moment = require('moment'); 
-var config = require('./config');
+var jwt = require("jwt-simple");
+var moment = require("moment");
+var config = require("./config");
 
-exports.ensureAuthenticated = function(req, res, next) { 
- if(!req.headers.authorization) {
- return res
- .status(403)
- .send({message: "Error"});
- }
+exports.ensureAuthenticated = function(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(403).send({ message: "Error" });
+  }
 
- var token = req.headers.authorization.split(" ")[1];
- var payload = jwt.decode(token, config.TOKEN_SECRET);
+  var token = req.headers.authorization.split(" ")[1];
+  var payload = jwt.decode(token, config.TOKEN_SECRET);
 
- if(payload.exp <= moment().unix()) {
- return res
- .status(401)
- .send({message: "The token expires"});
- }
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: "The token expires" });
+  }
 
- req.user = payload.sub;
- next();
-}
+  req.user = payload.sub;
+  next();
+};
